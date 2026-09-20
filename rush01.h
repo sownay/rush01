e5376f58d44956599a8feda0d0e70521dcd3dce5
Sown <143411347+sownay@users.noreply.github.com>
2026-09-20 08:34:41 +0200

    Refactor function names in rush01.h

--- rush01.h
+++ rush01.h
@@ -3,9 +3,13 @@
 
 # include <unistd.h>
 
-int		parse(char *str, int *clues);
-int		solve(int grid[4][4], int *clues);
-void	print_grid(int grid[4][4]);
-void	error(void);
+int		ft_parse(char *texte, int *regles);
+int		ft_solve(int tab[4][4], int *regles, int case_);
+int		ft_valid(int tab[4][4], int ligne, int colonne, int nombre);
+int		ft_visible_ligne(int *ligne, int inverse);
+int		ft_visible_colonne(int tab[4][4], int colonne, int inverse);
+int		ft_check(int tab[4][4], int *regles, int case_);
+void	ft_print(int tab[4][4]);
+void	ft_error(void);
 
 #endif
